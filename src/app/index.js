6294ed162869291595aaaff5b04fd0a82199a929
Victor Jimenez <vic.jicama@gmail.com>
2021-11-11 00:37:27 +0100

    Initial commit - Upload signed - Upload anonymous - Authorize - File listing

---
+++ src/app/index.js
@@ -0,0 +1,39 @@
+const init = (app, cxt) => {
+  const { domain, bucketPublic, bucketPrivate, instance } = cxt;
+
+  app.post("/app/signed-from-server/:mode", async function(req, res) {
+    const { mode } = req.params;
+    const { fileid, tags } = req.body;
+
+    const { form } = await instance.BucketFile.upload({
+      domain,
+      name: mode === "public" ? bucketPublic : bucketPrivate,
+      fileid,
+      tags
+    });
+
+    res.json({ form });
+  });
+
+  app.post("/app/authorize-private-from-server", async function(req, res) {
+    const token = await instance.Bucket.authorize({
+      domain,
+      name: bucketPrivate
+    });
+
+    res.json({ token });
+  });
+
+  app.get("/app/list/:mode", async function(req, res) {
+    const { mode } = req.params;
+
+    const list = await instance.BucketFile.list({
+      domain,
+      name: mode === "public" ? bucketPublic : bucketPrivate
+    });
+
+    res.json({ list });
+  });
+};
+
+module.exports = { init };
